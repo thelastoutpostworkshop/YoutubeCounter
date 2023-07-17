a48399863a6756f896ff858b71f9b0617e956003
charles <cgiguere@iname.com>
2023-07-17 16:58:40 -0400

    scheduler.h

--- YoutubeCounter.ino
+++ YoutubeCounter.ino
@@ -6,6 +6,7 @@
 #include "font/Aurebesh_Bold40pt7b.h"
 #include "counterWeb.h"
 #include "mp3tf16p.h"
+#include "scheduler.h"
 
 #ifdef __AVR__
 #include <avr/power.h>
@@ -69,57 +70,6 @@ enum Subscriber_Status
     GAINING,
 };
 Subscriber_Status currentSubscriberStatus = UNKNNOWN;
-
-// Task scheduler
-class Task {
-public:
-  typedef void (*taskCallback)(void);
-
-  // Default constructor
-  Task() : callback(nullptr), interval(0), lastRun(0) {}
-
-  Task(taskCallback cb, unsigned long intervalMillis) : callback(cb), interval(intervalMillis) {
-    lastRun = 0;
-  }
-
-  void runIfDue() {
-    unsigned long now = millis();
-    if (now - lastRun >= interval) {
-      lastRun = now;
-      callback();
-    }
-  }
-
-private:
-  taskCallback callback;
-  unsigned long interval;
-  unsigned long lastRun;
-};
-
-class TaskScheduler {
-public:
-  TaskScheduler() {
-    taskCount = 0;  // Initialize taskCount
-  }
-
-  void addTask(Task::taskCallback cb, unsigned long intervalMillis) {
-    if (taskCount < MAX_TASKS) {
-      tasks[taskCount++] = Task(cb, intervalMillis);
-    }
-  }
-
-  void runTasks() {
-    for (int i = 0; i < taskCount; ++i) {
-      tasks[i].runIfDue();
-    }
-  }
-
-private:
-  static const int MAX_TASKS = 10; // Max number of tasks that can be scheduled.
-  Task tasks[MAX_TASKS];
-  int taskCount;
-};
-
 
 TaskScheduler scheduler;
 
