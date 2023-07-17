519c101b565e329a02c662de7e1f6f0613c75e0d
charles <cgiguere@iname.com>
2023-07-16 20:28:52 -0400

    showRandomRoundPixels

--- YoutubeCounter.ino
+++ YoutubeCounter.ino
@@ -140,30 +140,33 @@ void initPixels(void)
     pixels.show();
 }
 
-void showRandomRoundPixels(void)
-{
-    static unsigned long lastUpdateTime = 0; // static variable to keep its value between calls
-    if (millis() - lastUpdateTime >= 10000)
-    { // 10000 milliseconds = 10 seconds
-        uint32_t randomColor = pixels.Color(random(256), random(256), random(256));
+void showRandomRoundPixels(void) {
+    // Get a random color
+    uint32_t randomColor = pixels.Color(random(256), random(256), random(256));
 
-        for (int i = 0; i < roundPixelsCount; i++)
-        {
-            pixels.setPixelColor(roundPixels[i], pixels.Color(0, 0, 0));
-        }
-        pixels.show();
-
-        // Create a for loop that runs 3 times or less if the roundPixelsCount is less than 3
-        int colorPixelCount = min(3, roundPixelsCount);
-        for (int i = 0; i < colorPixelCount; i++)
-        {
-            pixels.setPixelColor(roundPixels[i], randomColor);
-        }
-        pixels.show();
-
-        lastUpdateTime = millis(); // update the last update time
+    // Determine the number of pixels to change (up to 3)
+    int numPixelsToChange = min(3, roundPixelsCount);
+    
+    // Clear all pixels in roundPixels
+    for (int i = 0; i < roundPixelsCount; i++) {
+        pixels.setPixelColor(roundPixels[i], pixels.Color(0, 0, 0));
     }
+
+    // Show the cleared pixels
+    pixels.show();
+
+    // Change color of up to 3 random pixels
+    for (int i = 0; i < numPixelsToChange; i++) {
+        // Get a random index within the roundPixels array
+        int index = random(roundPixelsCount);
+        // Set the color of the pixel at the random index
+        pixels.setPixelColor(roundPixels[index], randomColor);
+    }
+
+    // Show the newly colored pixels
+    pixels.show();
 }
+
 
 void clearScreen(void)
 {
